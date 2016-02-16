2aff3f84d9e4cd63b3b413b23b37c2f9ef43280a
AndersBuhl <anders.buhl94@gmail.com>
2016-02-16 16:47:13 +0100

    Added functionality for recordNbr

--- Hospital/src/Util/Record.java
+++ Hospital/src/Util/Record.java
@@ -1,7 +1,8 @@
 package Util;
 
 public class Record {
-	private recordId;
+	private static int nbrRecords = 0;
+	private int recordNbr;
 	private Patient patient;
 	private Doctor doctor;
 	private Nurse nurse;
@@ -9,13 +10,19 @@ public class Record {
 	private String data;
 	
 	public Record(Patient patient, Doctor doctor, Nurse nurse, Division division, String data) {
+		this(patient, doctor, nurse, division, data, nbrRecords);		
+	}
+	
+	public Record(Patient patient, Doctor doctor, Nurse nurse, Division division, String data, int recordNbr) {
 		this.patient = patient;
 		this.doctor = doctor;
 		this.nurse = nurse;
 		this.division = division;
 		this.data = data;
+		this.recordNbr = recordNbr;
+		nbrRecords++;
+		
 	}
-	
 	public String getRecord(Person p) {
 		if(p.equals(patient) || p.equals(nurse) || p.equals(doctor) || p.getDivision().equals(division))
 			return data;
