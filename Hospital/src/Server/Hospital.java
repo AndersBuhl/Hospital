b1d761773d82628aefb07678aa5ab32984ceffd1
HerrAndersson95 <dic14jan@student.lu.se>
2016-02-18 15:47:55 +0100

    Somethings For the system

---
+++ Hospital/src/Server/Hospital.java
@@ -0,0 +1,42 @@
+package Server;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Hospital {
+	
+	public Hospital(){
+		genRecords();
+	}
+	
+	public void readInput(String input){
+		
+	}
+	
+	private void genRecords(){
+		String filePath = new File("").getAbsolutePath();
+        System.out.println (filePath);
+        BufferedReader reader;
+
+        try
+        {                    
+        	reader = new BufferedReader(new FileReader(filePath + "/src/Util/RecordsTest.txt"));
+            String line = null;         
+            while ((line = reader.readLine()) != null)
+            {
+                if (!(line.startsWith("*")))
+                {
+                    System.out.println(line);
+                }
+            }        
+            reader.close();
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }                 
+	}
+	
+}
---
+++ Hospital/src/Util/MainTest.java
@@ -0,0 +1,13 @@
+package Util;
+
+import Server.Hospital;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		System.out.println("Test Begins");
+		Hospital host = new Hospital();
+		
+	}
+
+}
